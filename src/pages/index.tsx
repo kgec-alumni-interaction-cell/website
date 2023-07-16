a50e8d932df860e8f0a3ed321a297682b2e2a7d5
asrvd <asheeshh@duck.com>
2023-07-16 12:57:34 +0530

    feat: navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,12 @@ import HomeHero from "@/components/home/hero";
 import HomeAbout from "@/components/home/about";
 import HomeMNV from "@/components/home/mnv";
 import HomeContact from "@/components/home/contact";
+import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
     <main className="min-h-screen min-w-screen flex flex-col">
-      <div className="h-12 p-2 bg-zinc-400 flex justify-between items-center sticky top-0">
-        <p>navbar</p>
-      </div>
+      <Navbar />
       <HomeHero />
       <HomeAbout />
       <HomeMNV />
