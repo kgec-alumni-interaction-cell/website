f93c8f81f752591af0627e6d4d83d5436cb0290f
shahank42 <crazykavlogs@gmail.com>
2023-07-17 15:02:31 +0530

    feat: added pages

---
+++ src/pages/digitaldb/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "@/components/layout";
+
+function DigitalDB() {
+  return (
+    <Layout>
+      DigitalDB
+    </Layout>
+  )
+}
+
+export default DigitalDB;
---
+++ src/pages/scholarships/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "@/components/layout";
+
+function Scholarships() {
+  return (
+    <Layout>
+      Scholarships
+    </Layout>
+  )
+}
+
+export default Scholarships;
