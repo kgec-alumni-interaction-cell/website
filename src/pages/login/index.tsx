d7a708418a81807e51f921504f70028d5bd541de
shahank42 <crazykavlogs@gmail.com>
2023-07-19 22:01:47 +0530

    feat: added the login and signup pages

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "@/components/layout";
+
+function Login() {
+  return (
+    <Layout>
+      Login
+    </Layout>
+  )
+}
+
+export default Login;
---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "@/components/layout";
+
+function SignUp() {
+  return (
+    <Layout>
+      SignUp
+    </Layout>
+  )
+}
+
+export default SignUp;
