6809bbecf3577f514d8161883d725d74c5e2c926
shahank42 <crazykavlogs@gmail.com>
2023-07-22 02:09:04 +0530

    feat: added mnv text

--- src/components/home/mnv/index.tsx
+++ src/components/home/mnv/index.tsx
@@ -1,7 +1,40 @@
 export default function HomeMNV() {
-    return (
-      <section className="h-[calc(100vh-12px)] bg-zinc-100 min-w-full flex flex-col">
-        <p>Mission & Vision</p>
-      </section>
-    );
-  }
+  return (
+    <section className=" bg-zinc-100 min-w-full flex flex-col p-8 lg:p-24">
+      <p className="text-[3rem]">Mission & Vision</p>
+
+      <ul className="text-lg flex flex-col gap-7">
+        <li>
+          Kalyani Government Engineering College (KGEC) in West Bengal was
+          established in 1995 and is affiliated with Maulana Abul Kalam Azad
+          University of Technology, Kolkata. It is renowned as one of the top
+          engineering colleges in the country, offering B.Tech, M.Tech, and MCA
+          courses across ten academic departments.
+        </li>
+        <li>
+          KGEC fosters a thriving community of eager learners, emphasizing both
+          academic and personal growth. The college's outcome-based education
+          approach puts students at the center of the learning process. The
+          campus spans 75 acres and features state-of-the-art infrastructure,
+          including seminar halls, E-classrooms, and a well-equipped library.
+        </li>
+        <li>
+          The college encourages industry-academia interaction through
+          workshops, guest lectures, and industrial visits. KGEC boasts robust
+          placement opportunities, with top-notch companies recruiting final
+          year students annually. Students actively participate in coding
+          competitions, hackathons, and events organized by various clubs,
+          including the Entrepreneurship Cell.
+        </li>
+        <li>
+          Faculty members are involved in research activities and mentor PhD
+          students. The college takes pride in its national-level journal,
+          'REASON-A Technical Journal.' Preparations for NAAC Accreditation are
+          underway, as KGEC strives for excellence in education, research, and
+          overall growth, empowering students to shine in the world of
+          engineering.
+        </li>
+      </ul>
+    </section>
+  );
+}
