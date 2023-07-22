9ff1c925f61e3431130db8b3fa896fd0237bd571
karmakarmeghdip <karmakarmeghdip@gmail.com>
2023-07-22 12:15:55 +0530

    added prisma adapter

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,12 +1,15 @@
 import NextAuth from "next-auth";
 import LinkedInProvider from "next-auth/providers/linkedin";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export const authOptions = {
-  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
   providers: [
     LinkedInProvider({
       clientId: process.env.LINKEDIN_CLIENT_ID,
-
       clientSecret: process.env.LINKEDIN_CLIENT_SECRET
     })
   ]
