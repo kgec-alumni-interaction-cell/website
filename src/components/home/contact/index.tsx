6d68ea62f4fa4832afac475331fcc1bc12c0ef12
shahank42 <crazykavlogs@gmail.com>
2023-08-03 16:19:37 +0530

    feat: updated the contact us section

--- src/components/home/contact/index.tsx
+++ src/components/home/contact/index.tsx
@@ -1,7 +1,36 @@
 export default function HomeContact() {
-    return (
-      <section className="h-[calc(100vh-12px)] bg-zinc-50 min-w-full flex flex-col">
-        <p>Contact</p>
-      </section>
-    );
-  }
+  return (
+    <section className=" bg-zinc-50 min-w-full flex flex-col p-8 lg:p-24">
+      <h2 className="text-[2rem] lg:text-[3rem] font-black mb-4 leading-none">
+        Contact Us
+      </h2>
+      <p>
+        Have questions or want to get in touch with the Alumni Association? We
+        are here to help!
+      </p>
+      <p>Reach out to us through the following means:</p>
+      <table className="table-fixed border my-7">
+        <tbody>
+          <tr className="border-b">
+            <td className="border-r py-2 px-3">Shubhankar Das</td>
+            <td className="py-2 px-3">+91 9064632186</td>
+          </tr>
+          <tr className="border-b">
+            <td className="border-r py-2 px-3">Arbendu Pratihar</td>
+            <td className="py-2 px-3">+91 8597058785</td>
+          </tr>
+          <tr className="border-b">
+            <td className="border-r py-2 px-3">Samridhdha Mitra </td>
+            <td className="py-2 px-3">+91 9804443065</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <p>
+        As an alumnus of KGEC, you are automatically part of our community. We
+        encourage you to become an active member and contribute to the legacy of
+        excellence.
+      </p>
+    </section>
+  );
+}
