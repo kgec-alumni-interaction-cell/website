b7dddfc0c5ea166ed19090643f02565f9da32478
shahank42 <shounakghosh42@gmail.com>
2023-12-31 00:45:36 +0530

    student registration works

--- src/pages/alumnibase/index.tsx
+++ src/pages/alumnibase/index.tsx
@@ -3,10 +3,11 @@ import Layout from "@/components/layout";
 import AlumniBase from "./AlumniBase";
 import AlumniBaseInfo from "./AlumniBaseInfo";
 import Alumni from "@/types/types";
+import { useEffect, useState } from "react";
 
 export async function getServerSideProps() {
   const res = await fetch(
-    "https://kgec-alumni-website.vercel.app/api/alumni/list"
+    "https://kgec-alumni-backend.onrender.com/users/alumni"
   );
   const alumniList = await res.json();
   return {
@@ -19,23 +20,40 @@ export async function getServerSideProps() {
 interface Props {
   alumniList: Alumni[];
 }
+// const [signedInUser, setSignedInUser] = useState<string>(
+//   (typeof window !== "undefined" &&
+//   window.localStorage.getItem("signed-in-user")
+//     ? window.localStorage.getItem("signed-in-user")
+//     : "") as string
+// );
 
 function AlumiBase({ alumniList }: Props) {
   // const { data: session } = useSession();
-  const session = 0
+  // const session = 0
+  const [signedInUser, setSignedInUser] = useState<string>("");
+
+  useEffect(() => {
+    if (typeof window !== "undefined")
+      if (window.localStorage.getItem("signed-in-user") !== null)
+        setSignedInUser(
+          window.localStorage.getItem("signed-in-user") as string
+        );
+
+    console.log(signedInUser)
+  }, []);
 
   // This is shown if not logged in
-  if (!session)
+  if (signedInUser === null)
     return (
       <Layout>
         <AlumniBaseInfo />
       </Layout>
     );
 
-  if (session)
+  if (signedInUser !== "")
     return (
       <Layout>
-        <AlumniBase alumniList={alumniList || ([] as Alumni[])} />
+        <AlumniBase alumniList={alumniList ? alumniList : ([] as Alumni[])} />
       </Layout>
     );
 }
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,15 +1,16 @@
 import Layout from "@/components/layout";
 import Link from "next/link";
-import { FormEvent } from "react";
+import { FormEvent, useEffect, useState } from "react";
 
 interface FormType {
   name: string;
   email: string;
   alumni: boolean;
-  gradYr: string;
+  gradYr: number;
   proofOfGrad: string;
   currWorkplace: string;
   password: string;
+  image: string;
 }
 
 function Register() {
@@ -17,17 +18,53 @@ function Register() {
     event.preventDefault();
     const formData = Object.fromEntries(new FormData(event.currentTarget));
     let formObj: FormType = {} as FormType;
-    formObj = { ...formObj, ...formData }
-    if (formData.alumni) formObj.alumni = true; else formObj.alumni = false;
-    
-    const response = await fetch("/api/auth/register", {
-      method: "POST",
-      body: JSON.stringify(formData)
-    })
+    formObj.name = formData.name as string;
+    formObj.email = formData.email as string;
+    // formObj.alumni = formData.alumni as unknown as boolean;
+    formObj.gradYr = parseInt(formData.gradYr as string);
+    formObj.proofOfGrad = formData.proofOfGrad as string;
+    formObj.currWorkplace = formData.currWorkplace as string;
+    formObj.password = formData.password as string;
+    formObj.image = "";
+    if (formData.alumni) formObj.alumni = true;
+    else formObj.alumni = false;
 
-    console.log(formObj)
-    console.log(response)
+    console.log(formObj);
+
+    const response = await fetch(
+      "https://kgec-alumni-backend.onrender.com/users/register",
+      {
+        method: "POST",
+        body: JSON.stringify(formObj),
+      }
+    );
+
+    console.log(response);
+
+    if (response.status === 200) {
+      if (typeof window !== "undefined") {
+        localStorage.setItem("signed-in-user", formObj.name);
+        window.location.reload();
+      }
+    }
   }
+
+  const [graduated, setGraduated] = useState<boolean>(false);
+
+  const [signedInUser, setSignedInUser] = useState<string>("");
+
+  // useEffect(() => {
+  //   if (signedInUser) {}
+  // })
+
+  useEffect(() => {
+    if (typeof window !== "undefined")
+      if (window.localStorage.getItem("signed-in-user") !== "")
+        setSignedInUser(
+          window.localStorage.getItem("signed-in-user") as string
+        );
+  }, []);
+
   return (
     <Layout>
       <section className="bg-gradient-to-b from-violet-500 via-indigo-500 to-indigo-600 min-w-full flex flex-col">
@@ -93,63 +130,89 @@ function Register() {
                   </div>
                 </div>
 
+                {!graduated ? (
+                  <div className="flex gap-2">
+                    <div>
+                      <label
+                        htmlFor="gradYr"
+                        className="block text-sm font-medium leading-6 text-yellow-300"
+                      >
+                        Year of Graduation
+                      </label>
+                      <input
+                        name="gradYr"
+                        required
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+                ) : (
+                  <></>
+                )}
+
                 <label className="block text-sm font-medium leading-6 text-yellow-300">
-                  <input name="alumni" type="checkbox" /> I have graduated from
-                  KGEC
+                  <input
+                    name="alumni"
+                    type="checkbox"
+                    onChange={() => {
+                      setGraduated((prev) => !prev);
+                    }}
+                  />{" "}
+                  I have graduated from KGEC
                 </label>
 
-                <div className="flex gap-2">
-                  <div>
-                    <label
-                      htmlFor="gradYr"
-                      className="block text-sm font-medium leading-6 text-yellow-300"
-                    >
-                      Graduation Year
-                    </label>
-                    <input
-                      name="gradYr"
-                      required
-                      className="block w-28 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
+                {graduated ? (
+                  <>
+                    <div className="flex gap-2">
+                      <div>
+                        <label
+                          htmlFor="gradYr"
+                          className="block text-sm font-medium leading-6 text-yellow-300"
+                        >
+                          Year Graduated
+                        </label>
+                        <input
+                          name="gradYr"
+                          required
+                          className="block w-28 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                      </div>
 
-                  <div className="w-full">
-                    <label
-                      htmlFor="proofOfGrad"
-                      className="block text-sm font-medium leading-6 text-yellow-300"
-                    >
-                      Proof of Graduation
-                    </label>
-                    <input
-                      name="proofOfGrad"
-                      placeholder="Enter drive link..."
-                      required
-                      className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                    {/* <input
-                      type="file"
-                      name="small-file-input"
-                      id="small-file-input"
-                      className="block w-full border border-gray-200 shadow-sm rounded-lg text-sm bg-zinc-50 focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none file:border-0 file:me-4 file:py-2 file:px-4"
-                    /> */}
-                  </div>
-                </div>
+                      <div className="w-full">
+                        <label
+                          htmlFor="proofOfGrad"
+                          className="block text-sm font-medium leading-6 text-yellow-300"
+                        >
+                          Proof of Graduation
+                        </label>
+                        <input
+                          name="proofOfGrad"
+                          placeholder="Enter drive link..."
+                          required
+                          className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                      </div>
+                    </div>
 
-                <div>
-                  <label
-                    htmlFor="currWorkplace"
-                    className="block text-sm font-medium leading-6 text-yellow-300"
-                  >
-                    Current Workplace
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      name="currWorkplace"
-                      required
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
+                    <div>
+                      <label
+                        htmlFor="currWorkplace"
+                        className="block text-sm font-medium leading-6 text-yellow-300"
+                      >
+                        Current Workplace
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          name="currWorkplace"
+                          required
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  <></>
+                )}
 
                 <div>
                   <button
@@ -161,15 +224,9 @@ function Register() {
                 </div>
               </form>
 
-              <p className="mt-10 text-center text-sm text-white">
-                Already recieved a password?
-                <Link
-                  href="/login"
-                  className="pl-1 font-semibold leading-6 text-yellow-300 hover:text-yellow-200"
-                >
-                  Log in now!
-                </Link>
-              </p>
+              <span className="mt-10 text-center text-sm text-white">
+                {signedInUser && `Signed in as ${signedInUser}!`}
+              </span>
             </div>
           </div>
         </div>
