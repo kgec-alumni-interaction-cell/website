d5ab91d314fdbe38eba015e6ee3e599c7829c963
imdebamrita <imdebamrita@gmail.com>
2024-01-01 00:55:02 +0530

    verify done with accept, verify document and reject

--- src/pages/verify/index.tsx
+++ src/pages/verify/index.tsx
@@ -1,56 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 import Layout from "@/components/layout";
+import { Alumni, UserType } from "@/types/types";
 
-const users = [
-  {
-    id: "0",
-    name: "Deba",
-    email: "asafdsf",
-    image: "/android-chrome-384x384.png",
-    alumni: true,
-    gradYr: "2026",
-    proofOfGrad: "https://www.google.com",
-    currWorkplace: "KGEC",
-    token: "",
-    password: "",
-  },
-  {
-    id: "1",
-    name: "Debamrita Paul",
-    email: "imdeba",
-    image: "/android-chrome-384x384.png",
-    alumni: true,
-    gradYr: "2026",
-    proofOfGrad: "",
-    currWorkplace: "KGEC",
-    token: "",
-    password: "",
-  },
-];
+export async function getServerSideProps() {
+  const result_alumnis = await fetch(
+    "https://kgec-alumni-backend.onrender.com/users/alumni"
+  );
+  const alumnis = await result_alumnis.json();
+  const result_students = await fetch(
+    "https://kgec-alumni-backend.onrender.com/users/students"
+  );
+  const students = await result_students.json();
+  return {
+    props: {
+      alumnis,
+      students,
+    },
+  };
+}
 
-function Verify() {
+interface Props {
+  alumnis: UserType[];
+  students: UserType[];
+}
+
+function Verify({ alumnis, students }: Props) {
+  const [selectedTab, setSelectedTab] = useState<string>("student");
+
   return (
     <Layout>
-      <ul className="grid grid-cols-2 gap-0 text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
+      <ul className="grid grid-cols-2 gap-0 text-base font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
         <li className="">
-          <a
-            href="#"
-            aria-current="page"
-            className="inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500"
+          <button
+            onClick={() => {
+              setSelectedTab("student");
+            }}
+            className={`inline-block p-4 rounded-t-lg  ${
+              selectedTab == "student" && "bg-gray-800"
+            } text-blue-500 dark:hover:bg-gray-800 dark:hover:text-gray-300`}
           >
             Students
-          </a>
+          </button>
         </li>
         <li className="">
-          <a
-            href="#"
-            className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"
+          <button
+            onClick={() => {
+              setSelectedTab("alumni");
+            }}
+            className={`inline-block p-4 rounded-t-lg ${
+              selectedTab == "alumni" && "bg-gray-800"
+            }  text-blue-500 dark:hover:bg-gray-800 dark:hover:text-gray-300`}
           >
-            Alumnies
-          </a>
+            Alumnis
+          </button>
         </li>
       </ul>
       <table className="w-75 text-sm text-left text-gray-500 dark:text-gray-400 mx-10">
@@ -68,61 +73,103 @@ function Verify() {
           </tr>
         </thead>
         <tbody>
-          {users.map((user) => {
-            return (
-              <tr
-                className="bg-white border-b dark:bg-gray-900 dark:border-gray-700"
-                key={user.id}
-              >
-                <td
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+          {(selectedTab == "student" ? students : alumnis)
+            .filter((user) => !user.verified)
+            .map((user) => {
+              return (
+                <tr
+                  className="bg-white border-b dark:bg-gray-900 dark:border-gray-700"
+                  key={user.id}
                 >
-                  {user.id}
-                </td>
-                <td className="px-6 py-4 flex">
-                  <div className="mr-4">
-                    <Image
-                      src={user.image}
-                      width="75"
-                      height="75"
-                      alt="User Image"
-                    />
-                  </div>
-                  <div>
-                    <p>Name: {user.name}</p>
-                    <p>Email: {user.email}</p>
-                    <p>Gratuaion Year: {user.gradYr}</p>
-                    {user.alumni && (
-                      <p>Current Working Space: {user.currWorkplace}</p>
-                    )}
-                  </div>
-                </td>
-                <td className="px-6 py-4">
-                  <button
-                    type="button"
-                    className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+                  <td
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                   >
-                    Accept
-                  </button>
-                  <button
-                    type="button"
-                    className="focus:outline-none text-white bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
-                  >
-                    <a href={user.proofOfGrad} target="_blank">
-                      Verify
-                    </a>
-                  </button>
-                  <button
-                    type="button"
-                    className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
-                  >
-                    Reject
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
+                    {user.id}
+                  </td>
+                  <td className="px-6 py-4 flex">
+                    <div className="mr-4">
+                      <Image
+                        src={user.image}
+                        width="75"
+                        height="75"
+                        alt="User Image"
+                      />
+                    </div>
+                    <div>
+                      <p className="text-gray-300">Name: {user.name}</p>
+                      <p>
+                        Couse: {user.degree} - {user.department}
+                      </p>
+                      <p>Gratuaion Year: {user.gradYr}</p>
+                      <p>Email: {user.email}</p>
+                      <p>WhatsApp Number: {user.waNum}</p>
+
+                      {user.alumni && (
+                        <p>Current Working Space: {user.currWorkplace}</p>
+                      )}
+                      <a
+                        href={user.linkedin}
+                        target="_blank"
+                        className="text-blue-500 hover:text-blue-300"
+                      >
+                        Linked In
+                      </a>
+                    </div>
+                  </td>
+                  <td className="px-6 py-4">
+                    <button
+                      onClick={async () => {
+                        const response = await fetch(
+                          "https://kgec-alumni-backend.onrender.com/users/verify",
+                          {
+                            method: "POST",
+                            body: JSON.stringify({
+                              id: user.id,
+                              token: "b863fcc7a0f0be7c2fc1b83c2bd55c16",
+                            }),
+                          }
+                        );
+
+                        window.location.reload();
+                      }}
+                      type="button"
+                      className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+                    >
+                      Accept
+                    </button>
+                    <button
+                      type="button"
+                      className="focus:outline-none text-white bg-yellow-600 hover:bg-yellow-700 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+                    >
+                      <a href={user.proofOfGrad} target="_blank">
+                        Verify Document
+                      </a>
+                    </button>
+                    <button
+                      onClick={async () => {
+                        const response = await fetch(
+                          "https://kgec-alumni-backend.onrender.com/users/delete",
+                          {
+                            method: "POST",
+                            body: JSON.stringify({
+                              id: user.id,
+                              token: "b863fcc7a0f0be7c2fc1b83c2bd55c16",
+                            }),
+                          }
+                        );
+
+                        window.location.reload();
+                      }}
+                      type="button"
+                      className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+                    >
+                      Reject
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
         </tbody>
       </table>
     </Layout>
