03d9a9d4c0fcb6f51e2ac36ec7faa9481edc4233
shahank42 <shounakghosh42@gmail.com>
2023-12-31 16:07:54 +0530

    auth

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,4 +1,4 @@
-export default interface Alumni {
+export interface Alumni {
     id: string;
     name: string;
     image: string;
@@ -6,3 +6,20 @@ export default interface Alumni {
     worksAt: string;
     linkedinProfile: string;
   };
+
+export interface UserType {
+  id: string;
+  name: string;
+  email: string;
+  waNum: string;
+  linkedin: string;
+  password: string;
+  degree: string;
+  department: string;
+  alumni: boolean;
+  gradYr: number;
+  proofOfGrad: string;
+  currWorkplace: string;
+  image: string;
+  verified: boolean;
+}
